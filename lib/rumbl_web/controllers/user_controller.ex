eb0a81af3830147e6fcf6964e3631e3b32bd1c82
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-09-30 21:16:02 +0300

    fix error

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -7,8 +7,9 @@ defmodule RumblWeb.UserController do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
-  def show(conn, %{"id => id"}) do
-    users = Accounts.get_user_by(id)
-    render(conn, "show.html", users: users)
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
   end
 end
